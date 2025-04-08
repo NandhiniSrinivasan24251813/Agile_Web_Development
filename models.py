535aab05dc17b9d1008a4bcf46a6ac898468302a
TerranConfederacy <1692017857@qq.com>
2025-04-09 03:48:02 +0800

    debug

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-// Test Only
+# // Test Only
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
